80c9dafde9c3750569153c990b6ad30af98168ee
Wirless <selppkez@gmail.com>
2019-11-10 14:47:56 +0100

    Add files via upload

---
+++ actionstrap.lua
@@ -0,0 +1,19 @@
+local config =
+{
+makeExhaustion = 15, --make exhaust? put the exhaust time in seconds or false if it won't make exhaustion
+exhaustionStorage = 51627, 
+}
+
+
+function onUse(cid, item, fromPosition, itemEx, toPosition, pos)
+	if (exhaustion.get (cid, config.exhaustionStorage)) then
+	print("".. getCreatureName(cid) .." is exhausted from trap")
+	else
+	print("".. getCreatureName(cid) .. " Planted Trap")
+	TRAP = doCreateItem(2579, 1, getCreaturePosition(cid))
+	doSetItemSpecialDescription(TRAP, "Trap created by: ".. getCreatureName(cid) .. "")
+	exhaustion.set (cid, config.exhaustionStorage, config.makeExhaustion)
+	end
+	return true
+end
+	
